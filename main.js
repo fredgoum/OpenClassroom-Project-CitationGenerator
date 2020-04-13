41b2d79763dd05312547b13218a34bc39246add2
fredgoum <alfredgoumou@gmail.com>
2020-04-13 12:23:43 +0200

    rewrite of the main function in pure javascript

--- main.js
+++ main.js
@@ -19,69 +19,76 @@ const longSentences = [
     ["parce que nous le valons bien.", "parce qu'il est temps d'agir.", "même si se n'est pas facile."]
 ];
 
-$(document).ready(function() {
-  // Options title
-  $("#options").click(function() {
-    if($("#options").is(':checked')) {
-      $('.optionsTitle').html('Masquer les options');
+// Title options
+const titleOptions = document.getElementById('options');
+titleOptions.addEventListener('click', function() {
+    const title = document.getElementById('titleOptions');
+    if (titleOptions.checked) {
+      title.innerHTML = 'Masquer les options';
     } else {
-      $('.optionsTitle').html('Afficher les options');      
+      title.innerHTML = 'Afficher les options';
     }
-  });
-  // Generate quotes
-  $("#quotesGeneratorBtn").click(function() {
-      let quotesNbSelected = $('#quotesNb').val();
-      let quotesGeneratorSelected = $('#quotesGenerator').val();
+});
 
-      let generatorTexts = [];
-      if (quotesGeneratorSelected == 'shortSentencesGenerator') {
-        generatorTexts = shortSentences;
-      } else {
-        generatorTexts = longSentences;
-      }
-
-      // Get quotes to display
-      const quotes = [];
-      for (let i = 0; i < quotesNbSelected; i++) {
-        let quote = [];
-        for (let i = 0; i < generatorTexts.length; i++) {
-          let randomNumber = Math.floor(Math.random() * generatorTexts[i].length);
-          let randomWord = generatorTexts[i][randomNumber];
-          quote.push(randomWord)
-        }
-        quotes.push(quote.join(''));
-        console.log(quote.join(''));
-      }
-
-      // Display quotes
-      let quote ="";
-      jQuery.each(quotes, function(i, value) {
-          quote += value + "<br/><br/>";
-      });
-      $('.display-quotes').html(quote);
-
-      // Pronounce quotes
-      let speech = new SpeechSynthesisUtterance(quote);
-      speechSynthesis.cancel();
-      speech.lang = 'fr-FR';
-      speech.pitch = 1;
-      speech.rate = 1;
-      speech.voiceURI = 'native';
-      speech.volume = 1;
-      if($("#speechCitationsCheckbox").is(':checked')) {
-        speechSynthesis.speak(speech);
-      }
-    });
-
-    // Stop game so stop speech also
-    $("#stopGameBtn").click(function() {
-      speechSynthesis.cancel();
-    });
+// Generate quotes
+const quotesGeneratorBtn = document.getElementById('quotesGeneratorBtn');
+quotesGeneratorBtn.addEventListener('click', function() {
+    // get select options (number of quotes and type of generator)
+    const quotesNb = document.getElementById('quotesNb');
+    const quotesNbSelected= quotesNb.options[quotesNb.selectedIndex].value;
+    const quotesGenerator = document.getElementById('quotesGenerator');
+    const quotesGeneratorSelected= quotesGenerator.options[quotesGenerator.selectedIndex].value;
     
-    // Stop speech if we uncheck the speech checkbox option
-    $("#speechCitationsCheckbox").click(function() {
-      if($("#speechCitationsCheckbox").is(':not(:checked)')) {
-        speechSynthesis.cancel();
+    let sentences = [];
+    if (quotesGeneratorSelected == 'shortSentencesGenerator') {
+      sentences = shortSentences;
+    } else {
+      sentences = longSentences;
+    }
+
+    // Get quotes to display
+    const quotes = [];
+    for (let i = 0; i < quotesNbSelected; i++) {
+      let quote = [];
+      for (let i = 0; i < sentences.length; i++) {
+        let randomNumber = Math.floor(Math.random() * sentences[i].length);
+        let randomWord = sentences[i][randomNumber];
+        quote.push(randomWord)
       }
+      quotes.push(quote.join(''));
+      console.log(quote.join(''));
+    }
+
+    // Display quotes
+    let quotesSentence ="";
+    quotes.forEach(quote => {
+      quotesSentence += quote + "<br/><br/>";
     });
+    document.getElementById('display-quotes').innerHTML = quotesSentence;
+    
+    // Pronounce quotes
+    let speech = new SpeechSynthesisUtterance(quotesSentence);
+    speechSynthesis.cancel();
+    speech.lang = 'fr-FR';
+    speech.pitch = 1;
+    speech.rate = 1;
+    speech.voiceURI = 'native';
+    speech.volume = 1;
+    if (document.getElementById("speeckCheckbox").checked) {
+      speechSynthesis.speak(speech);
+    }
+});
+
+// Stop game so stop speech also
+const stopGameBtnElt = document.getElementById('stopGame');
+stopGameBtnElt.addEventListener('click', function() {
+    speechSynthesis.cancel();
+});
+
+// Stop speech if we uncheck the speech checkbox option
+const speeckCheckboxElt = document.getElementById('speeckCheckbox');
+speeckCheckboxElt.addEventListener('click', function() {
+    if (! document.getElementById("speeckCheckbox").checked) {
+      speechSynthesis.cancel();
+    }
 });
